d5e9a75418997355ca1182fb0e29dc949361ef8d
amiarynda <barcelamiab2@gmail.com>
2026-09-17 15:53:44 +0700

    feat(mobile): add app color theme

---
+++ client_blogapp/lib/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color.fromARGB(255, 248, 247, 246);
+  static const surface = Color(0xFFFFFCF5);
+  static const ink = Color.fromARGB(255, 125, 103, 81);
+  static const muted = Color(0xFF9A8878);
+
+  static const accent = Color(0xFF76563E);
+
+  static const softBrown = Color(0xFFEFE4D8);
+  static const line = Color(0xFFE2D8CA);
+  static const input = Color(0xFFF0ECE5);
+}
